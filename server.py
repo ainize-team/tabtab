38bbd734a381ca6c6a2a65411f9b9b58b026f839
Hanjeong Lee <gkswjdzz@naver.com>
2020-11-09 08:02:17 +0900

    fix port 8080=>80

--- server.py
+++ server.py
@@ -71,7 +71,7 @@ def healthCheck():
 
 
 if __name__ == "__main__":
-    # from waitress import serve
-    app.debug = True
-    # serve(app, host='0.0.0.0', port=80)
-    app.run(port=8080)
+    from waitress import serve
+    # app.debug = True
+    serve(app, host='0.0.0.0', port=80)   
+    # app.run(port=8080)
